a321860c306034a4c1d568cafc95dd04ef683570
saraparva <60186938+saraparva@users.noreply.github.com>
2020-02-15 14:24:19 +0100

    Add files via upload

---
+++ S1_Celsius_Fahrenheit.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float F,C;
+    cout<<"Intodueix la temperatura en graus Celsius: ";
+    cin>>C;
+    F=((C*9)/5)+32;
+    cout<<"La temperatura en graus Fahrenheit es: "<<F<<" F."<<endl;
+}
