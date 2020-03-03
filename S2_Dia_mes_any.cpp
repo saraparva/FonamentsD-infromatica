880248e54a194a10beae6f5a422822a7a390dee0
saraparva <60186938+saraparva@users.noreply.github.com>
2020-03-03 13:16:40 +0100

    Rename S2_dia_mes_any.cpp to S2_Dia_mes_any.cpp

--- S2_Dia_mes_any.cpp
+++ S2_Dia_mes_any.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
     int dia,mes,any;
     string MES;
-    cout<<"Itrodueaix el dia més i l'any en el format (dia-mes-any):";
+    cout<<"Itrodueaix el dia mÃ©s i l'any en el format (dia-mes-any):";
     cin>>dia>>mes>>any;
     cout<<"Dia: "<<dia<<endl;
     cout<<"Mes: "<<mes<<endl;
     cout<<"Any: "<<any<<endl;
     if (mes==1){MES="Gener";}
     else{if(mes==2){MES="Febrer";}
-        else{if(mes==3){MES="Març";}
+        else{if(mes==3){MES="MarÃ§";}
             else{if(mes==4){MES="Abril";}
                 else{if(mes==5){MES="Maig";}
                     else{if(mes==6){MES="Juny";}
