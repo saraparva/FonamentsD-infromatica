8dcaa45d8e8804f6e4bead1e3ab59b4ee999121c
saraparva <60186938+saraparva@users.noreply.github.com>
2020-03-03 13:19:44 +0100

    Update S2_Taula_multiplicar.cpp

--- S2_Taula_multiplicar.cpp
+++ S2_Taula_multiplicar.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int num,i,por;
+    int num,i;
     cout<<"Indica el numero (entre el 1 i el 10) de la taula de multiplicar que vols veure: "<<endl;
     cin>>num;
     if (num>=1 && num<=10){
         for (i=1;i<=10;i++){
-            por=num*i;
-            cout<<num<<'x'<<i<<'='<<por<<endl;
+            cout<<num<<'x'<<i<<'='<<num*i<<endl;
         }
     }
     else{
