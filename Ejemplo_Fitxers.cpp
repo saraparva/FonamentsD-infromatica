4ec9321871069eed829e155824de11c07600cbff
saraparva <60186938+saraparva@users.noreply.github.com>
2020-03-10 13:07:43 +0100

    Add files via upload

---
+++ Ejemplo_Fitxers.cpp
@@ -0,0 +1,14 @@
+//Programa que llegeix un fitxer d'una llista de nombres i escriu quantes vegades surt el 33
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+    ifstream llista("facil.txt");
+    int num,suma=0;
+    while(llista>>num){
+        if(num==33){
+            suma++;
+        }
+    }
+    cout<<"A la llista apareix el nombre 33 "<<suma<<" vegades."<<endl;
+}
