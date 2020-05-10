b60593db000372b65353f1068fffa3466ea28bf2
saraparva <60186938+saraparva@users.noreply.github.com>
2020-05-10 17:26:58 +0200

    Update S2_Invertir_numero.cpp

--- S2_Invertir_numero.cpp
+++ S2_Invertir_numero.cpp
@@ -2,21 +2,12 @@
 #include <math.h>
 using namespace std;
 int main(){
-    int num,numc,cifras=0,i,potencia,suma=0,cifra;
+    int num,numInvertido=0;
     cout<<"Escriu un numero: ";
     cin>>num;
-    numc=num;
-    while(numc!=0){
-        cifras++;
-        numc/=10;
-    }
-    cifras--;
-    for(i=cifras;i>=0;i--){
-        cifra=num%10;
-        potencia=pow(10,i);
-        cifra*=potencia;
-        suma+=cifra;
+    while(num!=0){
+        numInvertido=numInvertido*10+num%10;
         num/=10;
     }
-    cout<<"El teu numero al reves es: "<<suma<<endl;
+    cout<<"El teu numero al reves es: "<<numInvertido<<endl;
 }
