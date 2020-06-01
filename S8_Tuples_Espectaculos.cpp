f56ebdb3f688b7fa16b614a9ec105a85e064cb83
saraparva <60186938+saraparva@users.noreply.github.com>
2020-06-01 18:09:40 +0200

    Add files via upload

---
+++ S8_Tuples_Espectaculos.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct data{
+    int dia;
+    int mes;
+    int any;
+};
+
+struct espectacle{
+    string nom;
+    string tipus;
+    data datai;
+    data dataf;
+    float preu;
+};
+
+void guardar_informacio(espectacle &esp);
+
+bool validar_data(data dataobs, data datai, data dataf);
+
+void escriure_esp(espectacle &esp);
+
+int main(){
+    espectacle esp;
+    data dataobs;
+    bool trobat=false;
+    cout<<"Quina es la data de la que vols informacio?"<<endl;
+    cin>>dataobs.dia>>dataobs.mes>>dataobs.any;
+    guardar_informacio(esp);
+    while(esp.nom!="fi" && !trobat){
+        if(validar_data(dataobs, esp.datai, esp.dataf)){
+            escriure_esp(esp);
+        }
+        guardar_informacio(esp);
+    }
+}
+
+void guardar_informacio(espectacle &esp){
+    cout<<"Escriu l'informació sobre l'espectacle: nom, tipus, data d'inici, data de finalitzacio i preu."<<endl;
+    cin>>esp.nom;
+    while(esp.nom!="fi"){
+        cin>>esp.tipus>>esp.datai.dia>>esp.datai.mes>>esp.datai.any>>esp.dataf.dia>>esp.dataf.mes>>esp.dataf.any>>esp.preu;
+    }
+}
+
+bool validar_data(data dataobs, data datai, data dataf){
+    bool res=false;
+    if(dataobs.any>=datai.any && dataobs.any<=dataf.any){
+        if(dataobs.mes>=datai.mes && dataobs.mes<=dataf.mes){
+            if(dataobs.dia>=datai.dia && dataobs.dia<=dataf.dia){
+                res=true;
+            }
+        }
+    }
+    return res;
+}
