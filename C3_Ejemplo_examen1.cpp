ce59ce34441a89411aa5adc8d64965b8ad40c7e7
saraparva <60186938+saraparva@users.noreply.github.com>
2020-06-02 20:11:41 +0200

    Add files via upload

--- C3_Ejemplo_examen1.cpp
+++ C3_Ejemplo_examen1.cpp
@@ -1,5 +1,3 @@
-//Grau GREVA Control 4, 2019 Tardor Dimarts, 17 de Desembre
-
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -7,107 +5,121 @@ using namespace std;
 
 struct viatge{
     string nom, desti;
-    int dia, mes, dies;
+    int dia, mes, duracio;
     float preu;
 };
 
-void llegir_fitxer(vector<viatge> &viajes);
-void escriure_info(vector<viatge> &viajes, int opc, int i, int dia, int mes);
-void cercar_perdata(vector<viatge> &viajes, int diaviaje, int mesviaje, int dies, int dia, int mes);
-void cercar_perviatge(vector<viatge> &viajes, string nom_viatge, int dia, int mes);
+float calculimpost(int dia, int mes, int diahoy, int meshoy);
+void llegir_fitxer(vector<viatge> &via);
+void cercardata(vector<viatge> &via, int dia, int mes, int duracio, int opc, int diahoy, int meshoy);
+void cercarnom(vector<viatge> &via, string nom, int opc, int diahoy, int meshoy);
+void escriureviatge(vector<viatge> &via, int i, int opc, int diahoy, int meshoy);
 
 int main(){
-    int dia, mes, diaviaje, mesviaje, opc, num_dies;
+    int diahoy, meshoy, opc, diav, mesv, dura;
     string nom_viatge;
-    vector<viatge> viajes;
-    llegir_fitxer(viajes);
-    cout<<"Quin dia es?"<<endl;
-    cin>>dia>>mes;
-    cout<<"MENU D'OPCIONS DE BUSQUEDA"<<endl;
-    cout<<"1. Per data d'inici"<<endl;
-    cout<<"2. Per nom de viatge"<<endl;
+    vector<viatge> via;
+    llegir_fitxer(via);
+    cout<<"Escriu el dia i el mes d'avui."<<endl;
+    cin>>diahoy>>meshoy;
+    cout<<"MENU DE BUSQUEDA"<<endl;
+    cout<<"1. Busqueda per data d'inici"<<endl;
+    cout<<"2. Busqueda per nom de viatge"<<endl;
+    cout<<"3. Sortir"<<endl;
     cin>>opc;
     while(opc>=1 && opc<=2){
         switch(opc){
             case 1:
-                cout<<"Introdueix la data a la que vols viatjar i el numero de dies."<<endl;
-                cin>>diaviaje>>mesviaje>>num_dies;
-                cercar_perdata(viajes, diaviaje, mesviaje, num_dies, dia, mes);
+                cout<<"Dia: ";
+                cin>>diav;
+                cout<<" Mes: ";
+                cin>>mesv;
+                cout<<" Duració: ";
+                cin>>dura;
+                cercardata(via, diav, mesv, dura, opc, diahoy, meshoy);
                 break;
             case 2:
-                cout<<"Introdueix el nom del viatge.";
+                cout<<"Nom del viatge: ";
                 cin>>nom_viatge;
-                cercar_perviatge(viajes, nom_viatge, dia, mes);
+                cercarnom(via, nom_viatge, opc, diahoy, meshoy);
                 break;
             default:;
         }
     }
 }
 
-void llegir_fitxer(vector<viatge> &viajes){
-    viatge v;
-    ifstream fi("Ultiminutviatges.txt");
-    while(!fi.eof()){
-        fi>>v.nom>>v.desti>>v.dia>>v.mes>>v.dies>>v.preu;
-        viajes.push_back(v);
-    }
-}
-
-void escriure_info(vector<viatge> &viajes, int opc, int i, int dia, int mes){
-    int dia2, mes2, dies, interes=1;
-    int datahoy,dataviaje, tiempo;
-    datahoy=mes*30+dia;
-    dataviaje=viajes[i].mes*30+viajes[i].dia;
-    tiempo=dataviaje-datahoy;
-    if(tiempo<=14 && tiempo>=7){
-        interes=0.8;
-    }
-    else{if(tiempo<7){
-        interes=0.6;
-        }
-    }
-    
-    if(opc==1){
-        cout<<viajes[i].nom<<' '<<viajes[i].desti<<' '<<viajes[i].dia<<'/'<<viajes[i].mes<<' '<<viajes[i].dies<<" dies "<<(viajes[i].preu)*interes<<" euros."<<endl;
-    }
-    else{
-        dies=viajes[i].dia+viajes[i].dies;
-        if(dies>30){
-            dia2=dies-30;
-            mes2=viajes[i].mes+1;
+float calculimpost(int dia, int mes, int diahoy, int meshoy){
+    int inici, avui;
+    float impost;
+    inici=mes*30+dia;
+    avui=meshoy*30+diahoy;
+    if((inici-avui)<14){
+        if((inici-avui)<=7){ ///menys 7
+            impost=0.6;
         }
         else{
-            dia2=dies;
-            mes2=viajes[i].mes;
+            impost=0.8;
         }
-        cout<<viajes[i].nom<<": "<<viajes[i].desti<<" del "<<viajes[i].dia<<'/'<<viajes[i].mes<<" al "<<dia2<<'/'<<mes2<<' '<<(viajes[i].preu)*interes<<" euros."<<endl;
+    }
+    else{impost=1;}
+    return impost;
+}
+
+void llegir_fitxer(vector<viatge> &via){
+    ifstream fi("Ultiminutviatges.txt");
+    viatge v;
+    while(!fi.eof()){
+        fi>>v.nom>>v.desti>>v.dia>>v.mes>>v.duracio>>v.preu;
+        via.push_back(v);
     }
 }
 
-void cercar_perdata(vector<viatge> &viajes, int diaviaje, int mesviaje, int dies, int dia, int mes){
-    int i, opc=1, trobat=0;
-    long int dim=viajes.size();
+void cercardata(vector<viatge> &via, int dia, int mes, int duracio, int opc, int diahoy, int meshoy){
+    int i, compt=0;
+    long int dim=via.size();
     for(i=0;i<dim;i++){
-        if(viajes[i].dia==dia && viajes[i].mes==mes && viajes[i].dies==dies){
-            trobat++;
-            escriure_info(viajes, opc, i, dia, mes);
+        if(via[i].dia==dia && via[i].mes==mes && via[i].duracio==duracio){
+            compt++;
+            escriureviatge(via,i,opc, diahoy, meshoy);
         }
     }
-    if(trobat==0){
-        cout<<"No s'ha trobat cap oferta."<<endl;
+    if(compt==0){
+        cout<<"No hi ha cap viatge disponible."<<endl;
     }
 }
 
-void cercar_perviatge(vector<viatge> &viajes, string nom_viatge, int dia, int mes){
-    int i, opc=2, trobat=0;
-    long int dim=viajes.size();
+void cercarnom(vector<viatge> &via, string nom, int opc, int diahoy, int meshoy){
+    int i, compt=0;
+    long int dim=via.size();
     for(i=0;i<dim;i++){
-        if(viajes[i].nom==nom_viatge){
-            trobat++;
-            escriure_info(viajes, opc, i, dia, mes);
+        if(via[i].nom==nom){
+            compt++;
+            escriureviatge(via,i,opc,diahoy,meshoy);
         }
     }
-    if(trobat==0){
-        cout<<"No s'ha trobat cap oferta."<<endl;
+    if(compt==0){
+        cout<<"No hi ha cap viatge disponible."<<endl;
+    }
+}
+
+void escriureviatge(vector<viatge> &via, int i, int opc, int diahoy, int meshoy){
+    int diaf, mesf, aux;
+    float impost=calculimpost(via[i].dia, via[i].mes, diahoy, meshoy);
+    if(opc==1){
+        cout<<via[i].nom<<' '<<via[i].desti<<' '<<via[i].dia<<'/'<<via[i].mes<<' ';
+        cout<<via[i].duracio<<" dies "<<via[i].preu*impost<<" euros"<<endl;
+    }
+    else{
+        aux=via[i].dia+via[i].duracio;
+        if(aux>30){
+            diaf=aux-30;
+            mesf=via[i].mes+1;
+        }
+        else{
+            diaf=aux;
+            mesf=via[i].mes;
+        }
+        cout<<via[i].nom<<": "<<via[i].desti<<" del "<<via[i].dia<<'/'<<via[i].mes;
+        cout<<" al "<<diaf<<'/'<<mesf<<' '<<via[i].preu*impost<<" euros"<<endl;
     }
 }
