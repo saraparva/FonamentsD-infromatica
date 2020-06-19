ad62c1f064b5ef340e4d36d6bbc3a01c4f13cc7d
saraparva <60186938+saraparva@users.noreply.github.com>
2020-06-19 12:23:56 +0200

    Update PARCIAL2_FASE1.cpp

--- PARCIAL2_FASE1.cpp
+++ PARCIAL2_FASE1.cpp
@@ -1,70 +1,31 @@
-///*****************************************************************************************************
-//
-///    ES OBLIGATORIO LLENAR LA INFORMACION QUE SE SOLICITA A CONTINUACIÓN
-///
-///                                    EXAMEN FINAL -- 19 DE JUNIO
-//
-///    NOMBRE: Sara Pardo Valero
-///    DNI: 53644881A
-///    GRADO: Grefi
-///    GRUPO o PROFESOR: Marta Gatius
-//
-///                                             PROBLEMA 1
-//
-//
-
-///*****************************************************************************************************
-///                                          BIBLIOTECAS
-///*****************************************************************************************************
-
-
 #include <iostream>
 #include <fstream>
 #include <vector>
 using namespace std;
-
-///*****************************************************************************************************
-///                                      CONSTANTES, SI SE DECLARAN
-///*****************************************************************************************************
-
-//const int F=7; const int C=5;
 const int F=7; const int C=5;
 
 typedef int Matriz[F][C];
 
-///*****************************************************************************************************
-///                             SUBPROGRAMAS, O EN SU CASO, ENCABEZADOS (PROTOTIPOS)
-///*****************************************************************************************************
-
 void Leer(Matriz & X, Matriz & R);
 void Mostrar( Matriz & X);
-/// DECLARAR SUBPROGRAMA SOLICITADO
 void CalculoB(Matriz & A, Matriz & B);
-///*****************************************************************************************************
-///                                          PROGRAMA PRINCIPAL
-///*****************************************************************************************************
 
 int main(){
    int Res;
    Matriz A,B,R;
    Leer(A,R);
    Mostrar(A);
-/// INSERTAR  AQUI LA LLAMADA AL SUBPROGRAMA SOLICITADO PARA CALCULAR B
-    CalculoB(A,B);
+   CalculoB(A,B);
    cout<<endl;
    Mostrar(B);
-///                            COMPROBAR
-    for(int f=0;f<F;f++){
-        for (int c=0; c<C; c++){
-            Res=R[f][c]-B[f][c];
-        }
-    }
+   for(int f=0;f<F;f++){
+       for (int c=0; c<C; c++){
+           Res=R[f][c]-B[f][c];
+       }
+   }
    cout << endl << endl << "La suma resultado es: " << Res << endl;
 }
-///*****************************************************************************************************
-///               DESARROLLO DE SUBPROGRAMAS
-///*****************************************************************************************************
-///
+
 void Leer(Matriz & X, Matriz & R){
     ifstream cinF ("Prueba.txt");
     for(int f=0;f<F;f++)
@@ -72,14 +33,12 @@ void Leer(Matriz & X, Matriz & R){
     for(int f=0;f<F;f++)
        for (int c=0; c<C; c++) cinF >> R[f][c];
 }
-//
+
 void Mostrar(Matriz & X){
     for(int f=0;f<F;f++){
        for (int c=0; c<C; c++) cout <<"\t" << X[f][c] ;
        cout << endl;}
 }
-
-/// DESARROLLAR SUBPROGRAMA SOLICITADO
 
 void CalculoB(Matriz & A, Matriz & B){
     int f,c,sumaf, sumac;
